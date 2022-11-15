a0a41dfec8af33c246654b2dc6ad5ca6c95888e6
Damrongsak Reetanon <damrongs@gmail.com>
2022-11-15 16:23:34 +0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+resource "random_pet" "server_name" {
+  length    = 1
+}
+resource "aws_instance" "app_server" {
+  ami           = "ami-0bee6b4258f1faee4"
+  instance_type = "t2.micro"
+  tags = {
+          Name = random_pet.server_name.id
+  }
+}
+output "server_name" {
+   value = random_pet.server_name
+}

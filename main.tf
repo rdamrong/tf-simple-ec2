aec11b5c8fec76ff0d0776e9d5900a6e49438398
Damrongsak Reetanon <damrongs@gmail.com>
2024-05-06 21:22:31 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,14 @@
+variable "X" {}
+
 provider "aws" {
   region = "ap-southeast-1"
 }
-resource "random_pet" "server_name" {
-  length    = 1
-}
+
 resource "aws_instance" "app_server" {
   ami           = "ami-0bee6b4258f1faee4"
   instance_type = "t2.micro"
   tags = {
-          Name = random_pet.server_name.id
+          Name = format("jenkins%d",X)
   }
 }
-output "server_name" {
-   value = random_pet.server_name.id
-}
+

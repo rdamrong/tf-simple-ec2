e78ccdad8c57f951acb9bbc2a128e47ac158a6fb
Damrongsak Reetanon <damrongs@gmail.com>
2024-05-06 21:37:31 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "app_server" {
   ami           = "ami-0bee6b4258f1faee4"
   instance_type = "t2.micro"
   tags = {
-          Name = format("jenkins%s",X)
+          Name = format("jenkins%s",var.X)
   }
 }
 
